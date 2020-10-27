a570519813d7e5a07178674464427e09edf2a397
Smith <s.j.smith@accenture.com>
2020-10-27 16:50:01 -0400

    remove build

--- applications/api/build/resources/main/db/initialize/initialize.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO PLAYER (id, name) values (2, 'Iron Man');
-INSERT INTO PLAYER (id, name) values (1, 'Wonder Woman');
-INSERT INTO PLAYER (id, name) values (3, 'Wolverine');
-INSERT INTO PLAYER (id, name) values (4, 'Cat Woman');
-INSERT INTO PLAYER (id, name) values (5, 'Superman');
-INSERT INTO PLAYER (id, name) values (6, 'Black Panther');
-INSERT INTO PLAYER (id, name) values (7, 'Captain Marvel');
-INSERT INTO PLAYER (id, name) values (8, 'Deadpool');
--- applications/api/build/resources/test/sql/player.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO PLAYER(id, name) values (2, 'Iron Man');
-INSERT INTO PLAYER(id, name) values (1, 'Wonder Woman');
